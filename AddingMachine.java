32f273884788bbb04b832f0763839ee6cce5b9ea
Lynn Tran <mellifluouslimerance@users.noreply.github.com>
2019-10-08 16:17:47 -0700

    Update AddingMachine.java
    Edited Version

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,12 +1,21 @@
+/*
+ * LT Tran
+ * 1212395377
+ * This program will add and subtract 
+ * integers as well as return the total amount
+ * and document the history of inputs 
+*/
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
+	private String history;
 
 	public AddingMachine () {
 
 		total = 0;  // not needed - included for clarity
+		history = "0";
 
 	}
 
@@ -18,20 +27,34 @@ public class AddingMachine {
 
 	public void add (int value) {
 
+		total += value;
+		history = history + " + " + value;
+		
 	}
 
 	public void subtract (int value) {
+		
+		total -= value;
+		history = history + " - " + value;
 
 	}
 
 	public String toString () {
 
-		return "";
+		return history;
 
 	}
 
 	public void clear() {
+		
+		total = 0;  // not needed - included for clarity
+		history = "0";
+		
 
 	}
+
 }
+
+	
+	
 
